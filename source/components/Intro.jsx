d90b18140c1409dd06710f0c5d01466f8291696f
Ryan <ryangipsoong@gmail.com>
2025-08-22 23:57:21 +0800

    Added short description of myself for the 'Intro' component.

--- source/components/Intro.jsx
+++ source/components/Intro.jsx
@@ -4,6 +4,7 @@ export default function Intro() {
             <span>Hello!</span>
             <h1>Hi, I'm Ryan,</h1>
             <span></span>
+            <p>Detail-obsessed, cat father and occasional tech nerd who's passionate about harmonizing design and technology to create intuitive, meaningful user experiences.</p>
         </div>
     )
 }
