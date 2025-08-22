2b832298d0f359b5c47b48a57c26d267475f8916
Ryan <ryangipsoong@gmail.com>
2025-08-23 00:42:31 +0800

    Reworked 'Intro' component to have all three sections of the home page -> 1. Intro, 2. Experience, 3. Projects

--- source/components/Intro.jsx
+++ source/components/Intro.jsx
@@ -1,10 +1,21 @@
 export default function Intro() {
     return (
-        <div className='intro'>
-            <span>Hello!</span>
-            <h1>Hi, I'm Ryan,</h1>
-            <span></span>
-            <p>Detail-obsessed, cat father and occasional tech nerd who's passionate about harmonizing design and technology to create intuitive, meaningful user experiences.</p>
+        <div>
+            <div className='intro'>
+                <span>Hello!</span>
+                <h1>Hi, I'm Ryan,</h1>
+                <span></span>
+                <p>Detail-obsessed, cat father and occasional tech nerd who's passionate about harmonizing design and technology to create intuitive, meaningful user experiences.</p>
+            </div>
+
+            <div className='experience'>
+                <h2>Experience</h2>
+            </div>
+
+            <div className='projects'>
+                <h2>Projects</h2>
+            </div>
         </div>
+
     )
 }
